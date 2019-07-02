dc720265d36f56007aab053b52dabef3dfe4ebe6
William Lee <admin@mongkonglee.com>
2019-07-02 11:04:29 +0800

    add hash url function

--- controller/controller.go
+++ controller/controller.go
@@ -3,16 +3,73 @@ package controller
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
+	"github.com/lytics/base62"
 )
 
 func New() http.Handler {
 	r := mux.NewRouter()
-	r.HandleFunc("/", Hello)
+	r.HandleFunc("/", encode)
+	r.HandleFunc("/{hash}", redirect)
 	return r
 }
 
-func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello")
+func encode(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Fprintln(w, `
+		<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Url-Short</title>
+</head>
+<body>
+        <form action="/" method="post">
+                URL:<input type="text" name="url">
+                <input type="submit" value="Send">
+        </form>
+</body>
+</html>	
+		`)
+		return
+
+	case http.MethodPost:
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		url := strings.TrimSpace(r.PostFormValue("url"))
+		if url == "" {
+			return
+		}
+
+		if !strings.Contains(url, "http") {
+			url = "http://" + url
+		}
+
+		hashUrl := base62.StdEncoding.EncodeToString([]byte(url))
+		fmt.Fprintf(w, "<html>hash url: <a href=\"/%s\">%s</a></html>", hashUrl, hashUrl)
+		return
+
+	}
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	b, err := base62.StdEncoding.DecodeString(vars["hash"])
+	if err != nil {
+		http.Error(w, "ERROR", http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, string(b), http.StatusPermanentRedirect)
+	return
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/kmollee/url-short
 
 go 1.12
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/lytics/base62 v0.0.0-20180808010106-0ee4de5a5d6d
+	github.com/stretchr/testify v1.3.0 // indirect
+)
